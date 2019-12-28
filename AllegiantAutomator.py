fe4aabae5325425894bbccabf6498eb4b2a1734e
Jonathan <jtduong22@gmail.com>
2019-12-27 16:00:24 -0800

    [ AllegiantAutomator.py ] - added basic functionality for selecting departure city and destination city

--- AllegiantAutomator.py
+++ AllegiantAutomator.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 url = 'https://www.allegiantair.com/'
 
 ## Load Webpage ##
@@ -18,30 +19,74 @@ driver.implicitly_wait(10)
 driver.get(url)
 print('Load successful')
 
+wait = WebDriverWait(driver, 10)
+
 try:
+
+    ## Close popup ##
     try:
         print("Waiting for credit card popup . . .")
+
         # Wait until popup
 
-        wait = WebDriverWait(driver, 10)
         wait.until(ec.visibility_of(driver.find_element_by_class_name("credit-card-overlay-content")))
-
+        popup = driver.find_element_by_class_name("credit-card-overlay-content")
         print("Popup found! Closing popup. . .")
+
+        time.sleep(1)
 
         # Press Escape Button to close popup
 
         actions = ActionChains(driver)
         actions.send_keys(Keys.ESCAPE)
+        actions.send_keys(Keys.ESCAPE)
+        actions.send_keys(Keys.ESCAPE)
         actions.perform()
+
+        # wait.until(ec.invisibility_of_element_located(popup))
+        print("Closed popup!")
+        # wait.until(ec.invisibility_of_element(driver.find_element_by_class_name("credit-card-overlay-content")))
+
 
     # No popup found, continue with program
     except sel_exc.NoSuchElementException:
         print("No Popup found, continuing. . . ")
 
+    ## Select Departure Location ##
+    print("Searching for Departure input")
+    dep_location = 'Bellingham, WA / Vancouver, BC (BLI)'
+
+    # find departure (from) box
+    dep_box = driver.find_element_by_name('search_form[departure_city]')
+    print(f"Departure Box found! Selecting {dep_location}")
+
+    # interact with box
+    dep_box.click()
+    dep_box.send_keys(dep_location)
+    time.sleep(1)
+    dep_box.send_keys(Keys.ENTER)
+
+    ## Select Destination Location ##
+
+    print("Searching for Destination input")
+    des_location = "Las Vegas, NV (LAS)"
+
+    # find destination (to) box
+    des_box = driver.find_element_by_name("search_form[destination_city]")
+    print(f"Destination Box found! Selecting {des_location}")
+
+    # interact with box
+    des_box.click()
+    des_box.send_keys(des_location)
+    time.sleep(1)
+    des_box.send_keys(Keys.ENTER)
 
 except Exception as e:
-    print(f"Type {type(e)} Exception has occured: {e}")
+    print(f"Type {type(e)} Exception has occurred: {e}")
+    driver.quit()
+    exit(1)
+
 
 input("Press any button to continue . . .")
-driver.close()
+driver.quit()
 
