bc86de46073a9b8b1912f7522e69e64fb8986aa2
Jonathan <jtduong22@gmail.com>
2019-12-28 14:42:09 -0800

    [ AllegiantAutomator.py ] - added basic price parsing ability for departure and returning flight. Note: currently retrieves the default one set by AllegiantAir.com which is the cheapest. Also always assumes that there will be a second flight for now

--- AllegiantAutomator.py
+++ AllegiantAutomator.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 import time
 url = 'https://www.allegiantair.com/'
 
@@ -109,6 +110,83 @@ try:
     # select date
     print("Selecting January 27th")
     driver.find_element_by_id("ui-datepicker-0-0-27").click()
+
+    ## Submit
+    driver.find_element_by_id("submit-search").click()
+
+### PAGE 2 ###
+
+    ## Get price of departure flight
+
+    print("Waiting for page to load . . .")
+    # time.sleep(7)
+    wait.until(ec.visibility_of_element_located((By.CLASS_NAME, "calendar")))
+
+    print("Done loading! Looking for selected element")
+
+    radio_query = driver.find_element_by_id("departing").find_elements_by_name("departing-flight")
+    radio_button = None
+
+    for button in radio_query:
+        print(button.tag_name, ' ', button.is_selected(), ' ', type(button.is_selected()))
+        if button.is_selected() == True:
+            print("Found selected radio button! Continuing...")
+            radio_button = button
+            break
+
+    if radio_button == None:
+        raise Exception("No radio button selected???")
+    # radio_button = driver.find_element_by_name("departing-flight")
+
+    print("Finding price of flight...")
+    parent = radio_button.find_element_by_xpath('../../..')
+
+    # select the object containing the price
+    p_span = parent.find_element_by_xpath('.//span[2]/span/span[3]')
+    prices = p_span.text.split()
+
+    # get last price (ignores strike through price if doing round trip)
+    price = prices[len(prices) - 1]
+    dep_price = price[1:len(price)]   # get rid of dollar sign
+    print(f"The price is ${dep_price}")
+
+    ## Get price of Return flight
+
+    print("Looking for Return flight! Looking for selected element")
+
+    radio_query = driver.find_element_by_id("returning").find_elements_by_name("returning-flight")
+    radio_button = None
+
+    for button in radio_query:
+        print(button.tag_name, ' ', button.is_selected(), ' ', type(button.is_selected()))
+        if button.is_selected() == True:
+            print("Found selected radio button! Continuing...")
+            radio_button = button
+            break
+
+    if radio_button == None:
+        raise Exception("No radio button selected???")
+    # radio_button = driver.find_element_by_name("departing-flight")
+
+    print("Finding price of flight...")
+    parent = radio_button.find_element_by_xpath('../../..')
+
+    # select the object containing the price
+    p_span = parent.find_element_by_xpath('.//span[2]/span/span[3]')
+    prices = p_span.text.split()
+
+    # get last price (ignores strike through price if doing round trip)
+    price = prices[len(prices) - 1]
+    ret_price = price[1:len(price)]  # get rid of dollar sign
+    print(f"The price is ${ret_price}")
+
+    ## Continue on to next page
+    # driver.find_element_by_class_name("continue enabled").click()
+    button = driver.find_element_by_class_name("button-wrapper").find_element_by_tag_name("button")
+    button.click()
+
+
+### Page 3 ###
 
 
 except Exception as e:
