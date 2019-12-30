e73ad11908692841de2f11d6cab99af73bc4ec0f
Jonathan <jtduong22@gmail.com>
2019-12-29 17:42:50 -0800

    added comments to page_5.py

--- page_5.py
+++ page_5.py
@@ -4,25 +4,26 @@ from selenium.webdriver.common.by import By
 def parse_car_page(driver, wait, is_car_booked):
     car_price = '0.00'
 
+    # book car if true
     if is_car_booked:
+        # wait for page to complete loading
         print("Waiting for page to load. . .")
         wait.until(ec.presence_of_element_located((By.ID, "vehicles")))
 
+        # select first car
         print("Loaded. Selecting first car")
         car_chooser = driver.find_element_by_id("vendors")
         car_listing = car_chooser.find_element_by_xpath(".//div[2]/table/tbody/tr[1]/td[1]/span/a")
 
+        # get car price
         car_price = ''.join(x for x in car_listing.text if str.isnumeric(x) or x == '.')
         car_listing.click()
+
+        wait.until(ec.staleness_of((By.ID, "pricing")))
 
     print(f"added car price is ${car_price}")
 
     # Wait until button is enabled
-    # wait.until(ec.presence_of_element_located((By.ID, "vehicles")))
-    # wait.until(ec.invisibility_of_element_located((By.CLASS_NAME, "white-overlay")))
-    # wait.until_not(ec.visibility_of_element_located((By.CLASS_NAME, "white-overlay")))
-    # wait.until(ec.element_to_be_clickable((By.CLASS_NAME, "continue")))
-    # wait.until(ec.element_to_be_clickable(driver.find_element_by_id("transport").find_elements_by_xpath(".//div[3]/div/button")))
     wait.until(ec.element_to_be_clickable((By.XPATH, "//*[@id='transport']//div[3]/div/button")))
 
     print("Moving onto the next page\n")
